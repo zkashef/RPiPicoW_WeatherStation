223b5e9c6e6f7fee8601e398a4bc1ae278701f64
zkashef <zkashef@icloud.com>
2023-12-12 18:23:17 -0500

    deleted file

--- secrets.py
+++
@@ -1,7 +0,0 @@
-secrets = {
-    'test' : 'Who said that?',
-    'ssid' : 'MJ_HangingOnTheRim',
-    'password' : 'watercloset420',
-    'aio_username' : "zkashef",
-    'aio_key' : "aio_cTSi81Mbf8flHudNqhfMzvUARX7k"
-    }
